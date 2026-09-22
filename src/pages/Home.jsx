d41af858c4be3961441bceac7c2476dc8ee6b3d1
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-22 18:56:17 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <main className="min-h-screen w-full bg-[#f4f3f1]">
+      <Header />
+    </main>
+  );
+};
+
+export default Home;
